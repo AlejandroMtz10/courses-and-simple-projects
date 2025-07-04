54021493d197e6d8913635ba2643ba4d57cb62a1
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-07-04 14:33:58 -0600

    I updated the navbar and footer
    I will make some changes in the footer component about the information and other things to add and I will make the Home page with cards and description of this project

--- my_videogames/frontend/videogames/src/components/Footer/Footer.jsx
+++ my_videogames/frontend/videogames/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="bg-gray-800 text-white text-sm py-4 mt-10">
             <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-                <p>&copy; {new Date().getFullYear()} Alejandro Developer Todos los derechos reservados.</p>
+                <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
                 <div className="flex space-x-4 mt-2 md:mt-0">
                     <h2>My videogames</h2>
                 </div>
--- my_videogames/frontend/videogames/src/components/Navbar/Navbar.jsx
+++ my_videogames/frontend/videogames/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ThemeToggle from '../ThemeToggle';
+import { IoLogoGameControllerB } from "react-icons/io";
+import './Navbar.css'; // Import the CSS file for Navbar styles
 
 function Navbar() {
     return (
         <nav className="bg-gray-900 dark:bg-gray-900 p-4">
             <div className="flex justify-between items-center max-w-6xl mx-auto">
-                <Link to="/" className="text-white text-lg font-bold">🎮 My Videogames</Link>
-                <div className="flex space-x-4">
-                    <Link to="/videogames" className="text-white hover:text-yellow-400">Videojuegos</Link>
-                    <Link to="/consoles" className="text-white hover:text-yellow-400">Consolas</Link>
-                    <Link to="/clasifications" className="text-white hover:text-yellow-400">Clasificaciones ESRB</Link>
-                </div>
+                <ul className="hidden sm:flex gap-x-6">
+                    <Link to="/" className="text-white text-4xl font-bold"><IoLogoGameControllerB /></Link>
+                    <Link to="/videogames"><li className="font-bold text-2xl text-white hover:text-green-500 hover:underline">Videogames</li></Link>
+                    <Link to="/consoles"><li className="font-bold text-2xl text-white hover:text-green-500 hover:underline">Consoles</li></Link>
+                    <Link to="/clasifications"><li className="font-bold text-2xl text-white hover:text-green-500 hover:underline">ESRB</li></Link>
+                </ul>
                 <div>
                     <ThemeToggle />
                 </div>
